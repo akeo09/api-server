6db744e90ccedda41b37bca99f17e6b90ecb04fb
Alicia Keo <keoalicia09@gmail.com>
2024-02-01 17:03:10 -0500

    MongoDB

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
+require('dotenv').config({ debug: true});
+
 const express = require ('express')
+require('./db/mongoose')
+const cors = require('cors');
+const userRouter = require('./routers/user')
+
 const app = express ()
+
+app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.use(express.json())
+app.use(userRouter)
 
 //Set port to the PORT environment variable (if it is defined),
 //otherwise set it to 3000
 const port = process.env.PORT || 3000
-
-//Set up a default route ('') and return 'Hello World!' in the
-//response when requests are recieved
-app.get ('', (req, res) => {
-        res.send ('Snow Day!')
-})
-
-//Configure the server to listen for connections on the port.
-//Print to the console when ready for connections 
 app.listen (port, () => {
     console.log ('Server is up on port ' + port)
 })
