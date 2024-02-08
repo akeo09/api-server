55f4913aa1396dd6146dccd5dd117c7853e66e45
Alicia Keo <keoalicia09@gmail.com>
2024-02-07 23:06:37 -0500

    email verified

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth')
 const express = require('express')
 const User = require('../models/user')
 const { sendVerificationEmail } = require('../emails/account.js') 
@@ -15,14 +16,25 @@ router.post('/user', async (req, res) => {
     await user.save()
     const token = await user.generateAuthToken()
     
-    sendVerificationEmail(user.email, user.username)
+    sendWelcomeEmail(user.email, user.username, token)
     res.status(201).send(user)
   } 
   catch(error) {
     res.status(400).send(error)
   }
+})
 
+router.get('/user/verification', auth, async (req, res) => {
+  const user = req.user
+  const token = req.token
 
+  console.log(user)
+  console.log(token)
+
+  user.email_verified = true
+  user.save()
+  
+  res.send()
 })
 
 module.exports = router
