90bb0b5ad3cd3af88ae84bf4b5867bebfe10342e
Alicia Keo <keoalicia09@gmail.com>
2024-02-19 14:55:22 -0500

    debug

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
+/*
 console.log(`Connecting to ${process.env.MONGODB_URL}`)
 mongoose.connect(process.env.MONGODB_URL).catch((e) => {
     console.log(e)
 })
+*/
