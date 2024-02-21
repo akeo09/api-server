3d1d48b2c0ed38f050cafd66637f1d97a219ae34
Alicia Keo <keoalicia09@gmail.com>
2024-02-21 10:27:48 -0500

    debug

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ const userRouter = require('./routers/user')
 
 const app = express()
 
-/*
+
 app.use(cors())
+/*
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
