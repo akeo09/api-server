1af29ff8a16a7cc45ebbb5505a74091a0fa61a66
Alicia Keo <keoalicia09@gmail.com>
2024-02-21 10:42:09 -0500

    uncommented app.js

--- src/app.js
+++ src/app.js
@@ -9,18 +9,16 @@ const app = express()
 
 
 app.use(cors())
-/*
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
-*/
 
-//app.post("/", (req, res) => { res.send('hello') })
 
-//app.use(express.json())
-//app.use(userRouter)
+app.use(express.json())
+app.use(userRouter)
 
 //Set port to the PORT environment variable (if it is defined),
 //otherwise set it to 3000
